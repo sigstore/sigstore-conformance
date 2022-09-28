ff71550876c99800f0b990a1c963e9c3fa7fbde3
Alex Cameron <asc@tetsuo.sh>
2022-09-28 23:22:04 +1000

    Remove generated artifacts

--- test/functional/sign_verify.py
+++ test/functional/sign_verify.py
@@ -1,3 +1,5 @@
+import os
+
 from ..container import ClientReleaseContainer
 
 
@@ -11,8 +13,13 @@ def sign_verify(container: ClientReleaseContainer) -> None:
     if container.tag.startswith("cosign"):
         # TODO(alex): Figure out a better way to represent this. Maybe some kind
         # of ABC for each client that exposes a `sign` and `verify` property?
-        container.run("sign-blob --overwrite README.md")
+        container.run("sign-blob README.md")
         container.run("verify-blob README.md")
     else:
-        container.run("sign --overwrite README.md")
+        container.run("sign README.md")
         container.run("verify README.md")
+
+    # TODO(alex): Probably need some setup/teardown phase for each test where we
+    # setup/cleanup a fresh directory.
+    os.remove("README.md.crt")
+    os.remove("README.md.sig")
