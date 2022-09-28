8fd3282bca81f87a0546e8974046a2605f6b101d
Alex Cameron <asc@tetsuo.sh>
2022-09-28 18:04:05 +1000

    Create initial test runner script

---
+++ test/functional/__init__.py
@@ -0,0 +1,3 @@
+from .basic import basic
+
+__all__ = ["basic"]
---
+++ test/functional/basic.py
@@ -0,0 +1,35 @@
+import os
+
+import docker
+
+
+def basic(client: docker.DockerClient, tag: str) -> None:
+    # TODO(alex): Refactor this all into a helper API
+    volumes_mapping = {
+        os.getcwd(): {
+            "bind": "/mnt/vol1",
+            "mode": "rw",
+        }
+    }
+    env_mapping = {
+        "GITHUB_ACTIONS": os.getenv("GITHUB_ACTIONS", ""),
+        "ACTIONS_ID_TOKEN_REQUEST_TOKEN": os.getenv(
+            "ACTIONS_ID_TOKEN_REQUEST_TOKEN", ""
+        ),
+        "ACTIONS_ID_TOKEN_REQUEST_URL": os.getenv("ACTIONS_ID_TOKEN_REQUEST_URL", ""),
+    }
+
+    # Sign and verify the README
+    client.containers.run(
+        tag,
+        command="sign /mnt/vol1/README.md",
+        volumes=volumes_mapping,
+        environment=env_mapping,
+    )
+
+    client.containers.run(
+        tag,
+        command="verify /mnt/vol1/README.md",
+        volumes=volumes_mapping,
+        environment=env_mapping,
+    )
