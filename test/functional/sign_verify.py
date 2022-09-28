80a24152f4fb2f8322f099c57063f80479eff540
Alex Cameron <asc@tetsuo.sh>
2022-09-28 23:09:52 +1000

    Cosign uses a different command for signing blobs

--- test/functional/sign_verify.py
+++ test/functional/sign_verify.py
@@ -8,5 +8,11 @@ def sign_verify(container: ClientReleaseContainer) -> None:
     """
 
     # Sign and verify the README
-    container.run("sign README.md")
-    container.run("verify README.md")
+    if container.tag.startswith("cosign"):
+        # TODO(alex): Figure out a better way to represent this. Maybe some kind
+        # of ABC for each client that exposes a `sign` and `verify` property?
+        container.run("sign-blob --overwrite README.md")
+        container.run("verify-blob README.md")
+    else:
+        container.run("sign --overwrite README.md")
+        container.run("verify README.md")
