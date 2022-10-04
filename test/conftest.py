47af5e516e6eaf3db0d83022d614dc74c63f22d5
Alex Cameron <asc@tetsuo.sh>
2022-10-05 01:10:37 +1100

    Formatting

--- test/conftest.py
+++ test/conftest.py
@@ -2,10 +2,11 @@ import itertools
 
 from .matrix import ReleaseChannelChoice, SigstoreClientChoice
 
-
 _ALL_IMPLS = list(itertools.product(SigstoreClientChoice, ReleaseChannelChoice))
 
 
 def pytest_generate_tests(metafunc):
     if "impl_a" in metafunc.fixturenames and "impl_b" in metafunc.fixturenames:
-        metafunc.parametrize(["impl_a", "impl_b"], itertools.permutations(_ALL_IMPLS, 2))
+        metafunc.parametrize(
+            ["impl_a", "impl_b"], itertools.permutations(_ALL_IMPLS, 2)
+        )
