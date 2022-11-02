fff20e4357dab0b8c79a7ca39c87a8134d9b5bde
Alex Cameron <asc@tetsuo.sh>
2022-11-02 11:38:26 +1100

    Add more assertions to basic test

--- test/test_sign_verify.py
+++ test/test_sign_verify.py
@@ -8,10 +8,18 @@ def test_sign_verify(client: SigstoreClient) -> None:
     A basic test that signs and verifies an artifact for a given Sigstore
     client.
     """
+    artifact_path = Path("artifact.txt")
+    certificate_path = Path("artifact.txt.crt")
+    signature_path = Path("artifact.txt.sig")
+
+    assert artifact_path.exists
+    assert not certificate_path.exists
+    assert not signature_path.exists
+
     # Sign and verify the artifact.
-    client.sign(
-        Path("artifact.txt"), Path("artifact.txt.crt"), Path("artifact.txt.sig")
-    )
-    client.verify(
-        Path("artifact.txt"), Path("artifact.txt.crt"), Path("artifact.txt.sig")
-    )
+    client.sign(artifact_path, certificate_path, signature_path)
+
+    assert certificate_path.exists
+    assert signature_path.exists
+
+    client.verify(artifact_path, certificate_path, signature_path)
