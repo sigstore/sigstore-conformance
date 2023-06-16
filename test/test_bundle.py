00043df6ed6358be9de919abe066f39108fdb42c
Zach Steindler <steiza@github.com>
2023-06-16 14:56:58 -0400

    Restore original test order
    Signed-off-by: Zach Steindler <steiza@github.com>

--- test/test_bundle.py
+++ test/test_bundle.py
@@ -20,39 +20,6 @@ def test_verify(
     client.verify(materials, input_path)
 
 
-@pytest.mark.signing
-def test_sign_does_not_produce_root(
-    client: SigstoreClient, make_materials_by_type: _MakeMaterialsByType
-) -> None:
-    """
-    Check that the client does not produce a bundle that contains a root
-    certificate.
-    """
-
-    materials: BundleMaterials
-    input_path, materials = make_materials_by_type("a.txt", BundleMaterials)
-    assert not materials.exists()
-
-    # Sign for our input.
-    client.sign(materials, input_path)
-
-    # Parse the output bundle.
-    bundle_contents = materials.bundle.read_bytes()
-    bundle = Bundle().from_json(bundle_contents)
-
-    # Iterate over our cert chain and check for roots.
-    certs = bundle.verification_material.x509_certificate_chain
-    for cert in certs.certificates:
-        cert = x509.load_der_x509_certificate(cert.raw_bytes)
-
-        try:
-            constraints = cert.extensions.get_extension_for_class(x509.BasicConstraints)
-            assert not constraints.value.ca
-        # BasicConstraints isn't required to appear in leaf certificates.
-        except x509.ExtensionNotFound:
-            pass
-
-
 def test_verify_rejects_root(
     client: SigstoreClient, make_materials_by_type: _MakeMaterialsByType
 ) -> None:
@@ -65,10 +32,11 @@ def test_verify_rejects_root(
         "has_root_in_chain.txt", BundleMaterials
     )
 
-    with pytest.raises(ClientFail):
+    with pytest.raises(CalledProcessError):
         client.verify(materials, input_path)
 
 
+@pytest.mark.signing
 def test_sign_does_not_produce_root(
     client: SigstoreClient, make_materials_by_type: _MakeMaterialsByType
 ) -> None:
