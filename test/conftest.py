0a456a3b89e138d9d7c7424b11eaa3332c7df46c
Zach Steindler <steiza@github.com>
2023-07-25 16:02:08 -0400

    Fix imports
    Signed-off-by: Zach Steindler <steiza@github.com>

--- test/conftest.py
+++ test/conftest.py
@@ -6,12 +6,8 @@ from typing import Callable, Tuple, TypeVar
 
 import pytest  # type: ignore
 
-from .client import (
-    BundleMaterials,
-    SignatureCertificateMaterials,
-    SigstoreClient,
-    VerificationMaterials,
-)
+from .client import (BundleMaterials, SignatureCertificateMaterials,
+                     SigstoreClient, VerificationMaterials)
 
 _M = TypeVar("_M", bound=VerificationMaterials)
 _MakeMaterialsByType = Callable[[str, _M], Tuple[Path, _M]]
--- test/test_bundle.py
+++ test/test_bundle.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from subprocess import CalledProcessError
 from test.client import BundleMaterials, ClientFail, SigstoreClient
 from test.conftest import _MakeMaterialsByType
 
