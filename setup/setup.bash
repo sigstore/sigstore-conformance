3cc9a03c479b819c41770ba973d95ff70e739bce
William Woodruff <william@trailofbits.com>
2024-09-13 10:24:15 -0400

    action, setup: scaffolding
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- setup/setup.bash
+++ setup/setup.bash
@@ -12,6 +12,10 @@ if [[ "${0}" == "${BASH_SOURCE[0]}" ]]; then
   die "Internal error: setup harness was executed instead of being sourced?"
 fi
 
+# We expect cpython-release-tracker to have been checked out;
+# the unit tests expect to load its assets.
+[[ -d "${GITHUB_WORKSPACE}/cpython-release-tracker" ]] || die "cpython-release-tracker is not checked out!"
+
 # Check the Python version, making sure it's new enough (3.7+)
 # The installation step immediately below will technically catch this,
 # but doing it explicitly gives us the opportunity to produce a better
