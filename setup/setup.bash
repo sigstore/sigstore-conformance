565c59a67850cdb18977d96b74cb636ca976b086
Jussi Kukkonen <jkukkonen@google.com>
2025-04-22 20:11:03 +0300

    Install dependencies in a virtualenv
    This makes sure we don't pollute the main Python environment.
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@google.com>

--- setup/setup.bash
+++ setup/setup.bash
@@ -28,4 +28,5 @@ min_vers=$(cut -d '.' -f2 <<< "${vers}")
 
 [[ "${maj_vers}" == "3" && "${min_vers}" -ge 7 ]] || die "Bad Python version: ${vers}"
 
-python -m pip install --requirement "${GITHUB_ACTION_PATH}/requirements.txt"
+python3 -m venv sigstore-conformance-env
+./sigstore-conformance-env/bin/python -m pip install --requirement "${GITHUB_ACTION_PATH}/requirements.txt"
